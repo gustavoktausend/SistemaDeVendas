71e78f5ef443948263306deedc80ae743c152c7e
Gustavo Kring Tausendfreund <gustavoktausend@gmail.com>
2019-09-11 16:08:14 -0300

    Add files via upload

---
+++ appwebdso2/src/main/java/com/gustavokring/dso2/appwebdso2/Appwebdso2Application.java
@@ -0,0 +1,13 @@
+package com.gustavokring.dso2.appwebdso2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Appwebdso2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Appwebdso2Application.class, args);
+	}
+
+}
---
+++ appwebdso2/src/test/java/com/gustavokring/dso2/appwebdso2/Appwebdso2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gustavokring.dso2.appwebdso2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Appwebdso2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
